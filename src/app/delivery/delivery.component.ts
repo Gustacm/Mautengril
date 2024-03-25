361400aad57dc96b0068e670582b6b3cd7c26e67
Gustacm <gcastillo.dev@gmail.com>
2024-03-25 14:37:13 -0300

    nav

--- src/app/delivery/delivery.component.ts
+++ src/app/delivery/delivery.component.ts
@@ -2,12 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CartaService } from '../carta.service';
 import { CardsComponent } from '../cards/cards.component';
 import e from 'express';
+import { NavbarComponent } from '../navbar/navbar.component';
  
 @Component({
   selector: 'app-delivery',
   templateUrl: './delivery.component.html',
   styleUrls: ['./delivery.component.css'],
-  imports: [CardsComponent],
+  imports: [CardsComponent, NavbarComponent],
   standalone: true
 
 })
@@ -48,40 +49,40 @@ getUserLocation() {
       const longitude = position.coords.longitude;
       const latitudeTienda = -35.42863;
       const longitudeTienda = -71.65627;
-
+      // -35.42772301473541, -71.6563411
 
       //saber a cuantos esta el usuario de la tienda
-      const distance = this.getDistanceFromLatLonInKm(latitude, longitude, latitudeTienda, longitudeTienda);
+      const distance = this.getDistanceFromLatLonInMeters(latitude, longitude, latitudeTienda, longitudeTienda);
       //saber si esta a menos de 1 km de la tienda
       this.isNear(latitude, longitude, latitudeTienda, longitudeTienda);
 
-      console.log(`Latitude: ${latitude}, Longitude: ${longitude}, Distance: ${distance} km`);
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}, Distance: ${distance} m`);
     });
   } else {
     console.error("Geolocation is not supported by this browser.");
   }
 }
-// calcualar la distancia entre dos puntos
-  getDistanceFromLatLonInKm(lat1:any, lon1:any, lat2:any, lon2:any) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = this.deg2rad(lon2-lon1); 
-    var a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2)
-      ; 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c; // Distance in km
-    return Math.round(d);
-  }
+// calcular la distancia entre dos puntos
+getDistanceFromLatLonInMeters(lat1:any, lon1:any, lat2:any, lon2:any) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = this.deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return Math.round(d * 1000); // Convert to meters
+}
   
   deg2rad(deg:any) {
     return deg * (Math.PI/180)
   }
 // calcula  el  costo del delivery
   isNear(latitude:any, longitude:any, latitudeTienda:any, longitudeTienda:any) {
-    const distance = this.getDistanceFromLatLonInKm(latitude, longitude, latitudeTienda, longitudeTienda);
+    const distance = this.getDistanceFromLatLonInMeters(latitude, longitude, latitudeTienda, longitudeTienda);
     
     const distanceInMeters = distance * 1500 ;
     let message = '';
